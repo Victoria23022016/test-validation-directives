d2ce9861781a76f3d17ef3a769e3f8a4ca42ada7
Vika <svi.victoria@gmail.com>
2024-06-13 23:16:49 +0700

    add invalid-date validator + over-age validator

--- src/app/validators/invalid-date-validator.ts
+++ src/app/validators/invalid-date-validator.ts
@@ -1,29 +1,33 @@
 import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
-export function InvalidDateValidator(): ValidatorFn {
+export const InvalidDateValidator = (
+  minDateValue: string,
+  minDateErrorMessage: (controlValue: string) => string,
+  maxDateErrorMessage: (controlValue: string) => string
+): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors | null => {
     if (control.value) {
       const value = new Date(control.value);
       value.setHours(0, 0, 0, 0);
 
-      const minDate = new Date('1900-01-01');
+      const minDate = new Date(minDateValue);
       minDate.setHours(0, 0, 0, 0);
       const today = new Date();
       today.setHours(0, 0, 0, 0);
 
       if (value < minDate) {
         return {
-          lessThanMinDate: 'Дата не может быть раньше 01.01.1900',
+          lessThanMinDate: minDateErrorMessage(control.value),
         };
       }
 
       if (value > today) {
         return {
-          moreThanToday: `Дата не может быть позже ${control.value}`,
+          moreThanMaxDate: maxDateErrorMessage(control.value),
         };
       }
     }
 
     return null;
   };
-}
+};
